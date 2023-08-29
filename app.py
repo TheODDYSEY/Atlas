652efe8fc0307979b5bb9e16415dad1d09fec75d
TheODDYSEY <mosessabz07@gmail.com>
2023-08-29 17:28:41 +0300

    JSON refresher 🪟

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ stores = [
     }
 ]
 
+# JSON is just a long string that follows a specific format like the one above 
+#  a python dictionary is returned as a JSON
+
 @app.get("/store")  # http://127.0.0.1:5000/store
 
 def get_store():
