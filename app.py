50948cfbd749e3b0f82d321227e244984bf480f0
TheODDYSEY <mosessabz07@gmail.com>
2023-09-01 20:03:30 +0300

    Mod create function

--- app.py
+++ app.py
@@ -31,16 +31,32 @@ def create_store():
 # NEW FUNCTION
 # create stores and items
 @app.post("/item") #POST http://127.0.0.1:5005/store/My Store/item
-def create_item(name):
-    item_data= request.get_json()
-    if item_data["store_id"] not in stores:
-        abort(404,message="Store not found")
-    
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     item_id = uuid.uuid4().hex
-    item = {**item_data,"id":item_id}
+    item = {**item_data, "id": item_id}
     items[item_id] = item
-    
-    return item,201  
+
+    return item 
 
 
 # gets all items 
