6475bb3ad48eca49172e6c2acf1756610e7e0b38
TheODDYSEY <mosessabz07@gmail.com>
2023-09-01 20:04:09 +0300

    Mod create store func

--- app.py
+++ app.py
@@ -21,12 +21,21 @@ def get_stores():
 # NEW FUNCTION
 @app.post("/store") #POST http://127.0.0.1:5005/store
 def create_store():
-    store_data= request.get_json()
-    store_id = uuid.uuid4().hex
-    store = {**store_data,"id":store_id}
-    stores[store_id] = store
-    return store,201
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
 
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
 
 # NEW FUNCTION
 # create stores and items
