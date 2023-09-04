0fbb39415c25976dabf676f11e3439de22ecda6f
TheODDYSEY <mosessabz07@gmail.com>
2023-09-04 23:21:13 +0300

    Added put method

--- resources/item.py
+++ resources/item.py
@@ -16,9 +16,29 @@ class Item(MethodView):
             KeyError
     abort(404,message="Item not found")
     
+    
     def delete(self,item_id):
         try:
             del items[item_id]
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found.")   
+            
+    def put(Self,item_id):
+        item_data = request.get_json()
+    # There's  more validation to do here!
+    # Like making sure price is a number, and also both items are optional
+    # You should also prevent keys that aren't 'price' or 'name' to be passed
+    # Difficult to do with an if statement...
+        if "price" not in item_data or "name" not in item_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+            )
+        try:
+            item = items[item_id]
+            item |= item_data
+
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")            
