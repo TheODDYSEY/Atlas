575b4f1b9fed6c6adb4d2fe3f77c7943311e3303
TheODDYSEY <mosessabz07@gmail.com>
2023-09-04 22:33:59 +0300

    Added storelist path in kmethodview

--- resources/store.py
+++ resources/store.py
@@ -28,4 +28,20 @@ class StoresList(MethodView):
     def get(self):
         return {"stores":list(stores.values())}
     
-    def post(self)                
+    def post(self):  
+            store_data = request.get_json()
+            if "name" not in store_data:
+                 abort(
+                        400,
+                         message="Bad request. Ensure 'name' is included in the JSON payload.",
+
+                                )
+            for store in stores.values():
+                if store_data["name"] == store["name"]:
+                    abort(400,message=f"Store already exists")  
+                    
+            store_id = uuid.uuid4().hex
+            store = {**store_data, "id": store_id}
+            stores[store_id] = store
+    
+            return store              
