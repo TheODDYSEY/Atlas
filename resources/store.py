21feff854380fb2d661480bad07cd322d136d0c8
TheODDYSEY <mosessabz07@gmail.com>
2023-09-04 22:31:26 +0300

    Added get store routes in methodview

--- resources/store.py
+++ resources/store.py
@@ -3,3 +3,29 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint,abort
 from db import stores 
+
+
+blp = Blueprint("stores",__name__,description="Operations on Stores")
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    def get(self,store_id):
+        try: 
+            return stores[store_id]
+        except KeyError:
+            abort (404,message="Store not found.")
+            
+    def delete(self,store_id):
+        try:
+            return stores[store_id]
+            return {"Message":"Store deleted"}
+        except KeyError:
+            abort(404,message="Store not found.")    
+            
+            
+@blp.route("/store")
+class StoresList(MethodView):
+    def get(self):
+        return {"stores":list(stores.values())}
+    
+    def post(self)                
