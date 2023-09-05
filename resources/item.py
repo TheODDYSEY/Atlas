11d09e32ad87c4d1cb7a5bbc84458c67e25904ac
TheODDYSEY <mosessabz07@gmail.com>
2023-09-05 23:54:47 +0300

    Reformatted file

--- resources/item.py
+++ resources/item.py
@@ -1,37 +1,37 @@
 import uuid
 from flask import request
 from flask.views import MethodView
-from flask_smorest import Blueprint,abort
+from flask_smorest import Blueprint, abort
 
 from db import items
-from schema import ItemSchema,ItemUpdateSchema
+from schema import ItemSchema, ItemUpdateSchema
 
+blp = Blueprint("Items", __name__, description="Operations on Items")
 
-blp = Blueprint("Items",__name__,description="Operations on Items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
-    def get(self,item_id):
+    def get(self, item_id):
         try:
             return items[item_id]
         except:
             KeyError
-    abort(404,message="Item not found")
-    
-    
-    def delete(self,item_id):
+
+    abort(404, message="Item not found")
+
+    def delete(self, item_id):
         try:
             del items[item_id]
             return {"message": "Item deleted."}
         except KeyError:
-            abort(404, message="Item not found.")   
-            
-    def put(Self,item_id):
+            abort(404, message="Item not found.")
+
+    def put(Self, item_id):
         item_data = request.get_json()
-    # There's  more validation to do here!
-    # Like making sure price is a number, and also both items are optional
-    # You should also prevent keys that aren't 'price' or 'name' to be passed
-    # Difficult to do with an if statement...
+        # There's  more validation to do here!
+        # Like making sure price is a number, and also both items are optional
+        # You should also prevent keys that aren't 'price' or 'name' to be passed
+        # Difficult to do with an if statement...
         if "price" not in item_data or "name" not in item_data:
             abort(
                 400,
@@ -43,21 +43,22 @@ class Item(MethodView):
 
             return item
         except KeyError:
-            abort(404, message="Item not found.")            
-            
+            abort(404, message="Item not found.")
+
+
 @blp.route("/item")
 class ItemList(MethodView):
     def get(self):
-        return {"items":list(items.values())}
-    
+        return {"items": list(items.values())}
+
     @blp.arguments(ItemSchema)
     def post(self):
         item_data = request.get_json()
-  
+
         for item in items.values():
             if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
+                    item_data["name"] == item["name"]
+                    and item_data["store_id"] == item["store_id"]
             ):
                 abort(400, message=f"Item already exists.")
 
@@ -65,5 +66,4 @@ class ItemList(MethodView):
         item = {**item_data, "id": item_id}
         items[item_id] = item
 
-        return item,201
-                
+        return item, 201
--- resources/store.py
+++ resources/store.py
@@ -1,47 +1,47 @@
 import uuid
 from flask import request
 from flask.views import MethodView
-from flask_smorest import Blueprint,abort
-from db import stores 
+from flask_smorest import Blueprint, abort
+from db import stores
 
+blp = Blueprint("stores", __name__, description="Operations on Stores")
 
-blp = Blueprint("stores",__name__,description="Operations on Stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
-    def get(self,store_id):
-        try: 
-            return stores[store_id]
-        except KeyError:
-            abort (404,message="Store not found.")
-            
-    def delete(self,store_id):
+    def get(self, store_id):
         try:
             return stores[store_id]
-            return {"Message":"Store deleted"}
         except KeyError:
-            abort(404,message="Store not found.")    
-            
-            
+            abort(404, message="Store not found.")
+
+    def delete(self, store_id):
+        try:
+            return stores[store_id]
+            return {"Message": "Store deleted"}
+        except KeyError:
+            abort(404, message="Store not found.")
+
+
 @blp.route("/store")
 class StoresList(MethodView):
     def get(self):
-        return {"stores":list(stores.values())}
-    
-    def post(self):  
-            store_data = request.get_json()
-            if "name" not in store_data:
-                abort(
-                        400,
-                        message="Bad request. Ensure 'name' is included in the JSON payload.",
+        return {"stores": list(stores.values())}
 
-                                )
-            for store in stores.values():
-                if store_data["name"] == store["name"]:
-                    abort(400,message=f"Store already exists")  
-                    
-            store_id = uuid.uuid4().hex
-            store = {**store_data, "id": store_id}
-            stores[store_id] = store
-    
-            return store              
+    def post(self):
+        store_data = request.get_json()
+        if "name" not in store_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'name' is included in the JSON payload.",
+
+            )
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message=f"Store already exists")
+
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+
+        return store
