afd205c8f123835be45a7841c6ac6b55b84daf51
TheODDYSEY <mosessabz07@gmail.com>
2023-09-05 23:03:07 +0300

    Finishing app.py

--- app.py
+++ app.py
@@ -4,16 +4,26 @@
 # from db import items,stores
 
 from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
 
 app = Flask(__name__)
 
-app.config["PROPOGATE_ECEPTIONS"]=True
+app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "V1"
+app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] ="/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"]
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
 
 
 
