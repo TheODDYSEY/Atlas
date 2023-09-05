9125e409b44832f5c2208e38036770dd6b1c7f61
TheODDYSEY <mosessabz07@gmail.com>
2023-09-05 23:37:38 +0300

    Added store schema

--- schema.py
+++ schema.py
@@ -11,3 +11,9 @@ class ItemSchema(Schema):
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()        
+    
+    
+class StoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)    
+    
