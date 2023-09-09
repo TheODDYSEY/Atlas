489947606034549fe0df640681b92f4235ef4735
TheODDYSEY <mosessabz07@gmail.com>
2023-09-09 21:40:58 +0300

    Configuring sqlalchemy part 2

--- resources/store.py
+++ resources/store.py
@@ -2,7 +2,7 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+
 from schema import StoreSchema 
 
 blp = Blueprint("stores", __name__, description="Operations on Stores")
