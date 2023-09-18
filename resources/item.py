9f8561c1c19d6403bce6a8aed2a508892d37ec40
TheODDYSEY <mosessabz07@gmail.com>
2023-09-18 23:28:02 +0300

    FIN models in database

--- resources/item.py
+++ resources/item.py
@@ -20,21 +20,13 @@ class Item(MethodView):
         return abort(404, message="Item not found")
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented ")
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200,ItemSchema)
     def put(Self, item_data ,item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented ")
 
 
 @blp.route("/item")
--- resources/store.py
+++ resources/store.py
@@ -16,17 +16,12 @@ blp = Blueprint("stores", __name__, description="Operations on Stores")
 class Store(MethodView):
     @blp.response(200,StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            return stores[store_id]
-            return {"Message": "Store deleted"}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented")
 
 
 @blp.route("/store")
