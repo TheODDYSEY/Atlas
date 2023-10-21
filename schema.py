ce008c003f8d7b5abbb751c816780977eb63e2b3
TheODDYSEY <mosessabz07@gmail.com>
2023-10-21 13:59:47 +0300

    Added Tag schema

--- schema.py
+++ schema.py
@@ -12,6 +12,10 @@ class PlainStoreSchema(Schema):
     name = fields.Str()
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+    
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
@@ -25,3 +29,8 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+    
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
